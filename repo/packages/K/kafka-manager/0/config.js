8b31890ab8fd3a532b404d9a65181fb17e4ad734
Clement Laforet <sheepkiller@cotds.org>
2016-04-20 22:15:46 +0200

    fix config.js

--- repo/packages/K/kafka-manager/0/config.js
+++ repo/packages/K/kafka-manager/0/config.js
@@ -1,55 +1,54 @@
 {
-    "type": "object",
-    "properties": {
-        "kafka-manager": {
-            "description": "Kafka Manager configuration properties",
-            "type": "object",
-            "additionalProperties": false,
-
-            "properties": {
-                "app": {
-                    "description": "Marathon app configuration properties",
-                    "type": "object",
-                    "additionalProperties": false,
-                    "properties": {
-                        "cpus": {
-                            "description": "cpu requirements",
-                            "type": "number",
-                            "default": 0.5
-                        },
-                        "mem": {
-                            "description": "mem requirements (this should be approximately 120% of the heap size).",
-                            "type": "integer",
-                            "default": 512
-                        },
-                        "heap-mb": {
-                            "description": "Heap size for scheduler JVM, in MiB",
-                            "type": "integer",
-                            "default": 384
-                        },
-                        "instances": {
-                            "description": "app instances",
-                            "type": "integer",
-                            "default": 1
-                        },
-                        "application_secret": {
-                            "description": "The secret key is used to secure cryptographics functions",
-                            "type": "string",
-                            "default": "^<csmm5Fx4d=r2HEX8pelM3iBkFVv?k[mc;IZE<_Qoq8EkX_/7@Zt6dP05Pzea3U"
-                        }
-                    },
-                    "required": ["cpus", "mem", "instances", "heap-mb"]
-                },
-                "zk": {
-                    "type": "string",
-                    "description": "ZK URL for Kafka-manager",
-                    "default": "master.mesos:2181"
-                }
-            },
-            "required": [
-                "app",
-                "zk"
-            ]
+  "type": "object",
+  "properties": {
+    "kafka-manager": {
+      "description": "Kafka-manager configuration properties",
+      "type": "object",
+      "properties": {
+        "framework-name": {
+          "description": "The framework name to register with marathon.",
+          "type": "string",
+          "default": "kafka-manager"
+        },
+        "cpus": {
+            "description": "CPU shares to allocate.",
+            "type": "number",
+            "default": 1.0,
+            "minimum": 0.1
+        },
+        "mem": {
+            "description": "Memory to allocate.",
+            "type": "number",
+            "default": 1024.0,
+            "minimum": 1024.0
+        },
+        "zk": {
+            "type": "string",
+            "description": "ZK URL for Kafka-manager",
+            "default": "master.mesos:2181"
+        },
+        "instances": {
+            "description": "app instances",
+            "type": "integer",
+            "default": 1
+        },
+        "application_secret": {
+            "description": "The secret key is used to secure cryptographics functions",
+            "type": "string",
+            "default": "^<csmm5Fx4d=r2HEX8pelM3iBkFVv?k[mc;IZE<_Qoq8EkX_/7@Zt6dP05Pzea3U"
         }
+      },
+      "required": [
+        "framework-name",
+        "host",
+        "cpus",
+        "mem",
+        "instances",
+        "zk"
+      ]
     }
+  },
+  "required": [
+    "kafka-manager"
+  ]
 }
